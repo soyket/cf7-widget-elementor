e509d80c1bef86127268600563827a6f8c0afb91
Sayem <devssayem@gmail.com>
2020-07-15 14:54:41 +0600

    contact form editor and creator open in elementor editor

--- custom-editor/assets/js/void-cf7-elementor-editor.js
+++ custom-editor/assets/js/void-cf7-elementor-editor.js
@@ -7,21 +7,6 @@
         modalContainer,
         close,
         iframe;
-
-    // var modal = $('#cf7_widget_elementor_contact_form_control_modal'),
-    //     modalContainer = modal.find('.cf7-widget-elementor-modal-content'),
-    //     close = modal.find('.cf7-widget-elementor-modal-close'),
-    //     iframe = modal.find('.cf7-widget-elementor-modal-iframe');
-
-    // close.on('click', function(){
-    //     console.log('click hide event');
-    //     modal.hide();
-    // });
-
-    // var modalControl = function( $scope, $ ) {
-    //     tmp = $('#cf7_widget_elementor_contact_form_control_modal');
-    //     console.log();
-    // };
     
     var formIdCatch = function( $scope, $ ) {
 
@@ -32,37 +17,37 @@
         modalContainer = modal.find('.cf7-widget-elementor-modal-content');
         close = modal.find('.cf7-widget-elementor-modal-close');
         iframe = modal.find('.cf7-widget-elementor-modal-iframe');
-        console.log(formId);
 
         close.on('click', function(){
             modal.hide();
         });
 
-        };
+        // windowParent.jQuery('body').onclick = function(event) {
+        //     console.log('body click');
+        //     if (event.target == modal) {
+        //         modal.hide();
+        //     }
+        // }
+
+    };
 
     var eventCatchFunction = function (panel, model, view) {
         var $elementEdit = panel.$el.find( '.void-cf7-edit-form-btn' ).find( '#void-cf7-edit-form-btn' );
         $elementEdit.on('click', function(e){
             e.preventDefault();
-            //let modal = windowParent.jQuery('#cf7_widget_elementor_contact_form_control_modal');
+            iframe.attr('src', voidCf7Admin.url+'admin.php?page=wpcf7&post='+formId+'&active-tab=0');
             modal.show();
-            console.log('edit event Clicked!');
-            console.log(modal);
         });
 
         var $elementAdd = panel.$el.find( '.void-cf7-add-form-btn' ).find( '#void-cf7-add-form-btn' );
         $elementAdd.on('click', function(e){
             e.preventDefault();
-            //let modal = windowParent.jQuery('#cf7_widget_elementor_contact_form_control_modal');
+            iframe.attr('src', voidCf7Admin.url+'admin.php?page=wpcf7-new');
             modal.show();
-            console.log('add event Clicked!');
-            console.log(modal);
         });
     };
 
     $(window).on('elementor/frontend/init', function () {
-
-        //elementorFrontend.hooks.addAction( 'frontend/element_ready/global', modalControl );
 
         elementorFrontend.hooks.addAction( 'frontend/element_ready/void-section-cf7.default', formIdCatch);
         
--- custom-editor/modal-editor.php
+++ custom-editor/modal-editor.php
@@ -4,8 +4,7 @@
   <!-- Modal content -->
   <div class="cf7-widget-elementor-modal-content">
     <span class="cf7-widget-elementor-modal-close">&times;</span>
-    <!-- <iframe class="cf7-widget-elementor-modal-iframe" src="https://www.w3schools.com"></iframe> -->
-        <h1>Modal</h1>
+    <iframe class="cf7-widget-elementor-modal-iframe" ></iframe>
   </div>
 
 </div>
@@ -14,7 +13,7 @@
 #cf7_widget_elementor_contact_form_control_modal {
     position: fixed; /* Stay in place */
     z-index: 999; /* Sit on top */
-    padding-top: 100px; /* Location of the box */
+    padding-top: 50px; /* Location of the box */
     left: 0;
     top: 0;
     width: 100%; /* Full width */
@@ -31,6 +30,12 @@
     padding: 20px;
     border: 1px solid #888;
     width: 80%;
+    min-height: 720px;
+}
+
+.cf7-widget-elementor-modal-iframe{
+    width: 100%;
+    min-height: 720px;
 }
   
 /* The Close Button */
