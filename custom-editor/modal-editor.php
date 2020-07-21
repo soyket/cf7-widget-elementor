93cda11a9a3eb6bdd22c0e41b9eaad917d34e557
Sayem <devssayem@gmail.com>
2020-07-21 10:55:53 +0600

    hide all admin element from iframe

--- custom-editor/modal-editor.php
+++ custom-editor/modal-editor.php
@@ -6,24 +6,29 @@
     <span class="cf7-widget-elementor-modal-close">&times;</span>
     <!-- iframe to open contact form edit or add functionality -->
     <iframe onload="voidCf7IframeOnLoad()" class="cf7-widget-elementor-modal-iframe" ></iframe>
-    <button class="cf7-widget-elementor-modal-hide-adminmenu" onclick="voidCf7AdminMenuHide()">Try it</button>
   </div>
 </div>
 
 <script>
+// mneu hide wp admin menu function
 function voidCf7AdminMenuHide() {
   var iframe = document.getElementsByClassName("cf7-widget-elementor-modal-iframe");
   var adminMenu = iframe[0].contentWindow.document.getElementById("adminmenumain");
   var wpContent = iframe[0].contentWindow.document.getElementById("wpcontent");
+  var adminBar = iframe[0].contentWindow.document.getElementById("wpadminbar");
   if(adminMenu != null){
     adminMenu.style.display = "none";
+  }
+  if(adminBar != null){
+    adminBar.style.display = "none";
   }
   if(wpContent != null){
     wpContent.style.marginLeft = "0px";
   }
 }
-
+// iframe onload function
 function voidCf7IframeOnLoad(){
+  // call hide admin menu function after load iframe
   voidCf7AdminMenuHide();
 }
 </script>
