324be36fca2c21d029edaca64a4f56175d8e6bf9
sayedulsayem <devssayem@gmail.com>
2020-07-26 15:39:38 +0600

    widget switch data loose issue solve

--- custom-editor/assets/js/void-cf7-elementor-editor.js
+++ custom-editor/assets/js/void-cf7-elementor-editor.js
@@ -19,15 +19,52 @@
 
     // elementor edit panel call back function from hook
     var addEditActionFunction = function (panel, model, view) {
-        // form edit button element selector
-        let $elementEdit = panel.$el.find( '.void-cf7-edit-form-btn' ).find( '#void-cf7-edit-form-btn' );
         // elementor update preview button selector
         var elUpdatePreviewButton = panel.$el.find('.elementor-update-preview');
         // hide button from edit panel
         elUpdatePreviewButton.hide();
+
+        // call initial form assign function for preventing data loose after switching widget
+        addButtonFunction();
+
+        //when moving from Advanced tab to content model variable is null so to pass it's data
+        function pass_around_model(panel,model,view){
+            addButtonFunction();
+        }
+
+        // form edit button element selector
+        var $elementEdit = panel.$el.find( '.void-cf7-edit-form-btn' ).find( '#void-cf7-edit-form-btn' );
+        
         // form edit button click event function
         $elementEdit.on('click', function(e){
+            editButtonFunction();
+        });
+
+        // form add new button element selector
+        var $elementAdd = panel.$el.find( '.void-cf7-add-form-btn' ).find( '#void-cf7-add-form-btn' );
+        // form add new button click event function
+        $elementAdd.on('click', function(e){
             e.preventDefault();
+            // loader add on modal
+            modalLoading.addClass('loading');
+            // insert src in iframe with edit link of selected form
+            iframe.attr('src', voidCf7Admin.url+'admin.php?page=wpcf7-new');
+            // set opacity 0 to hide iframe untill it's load contents, opacity will be 1 after it's load content from modal-editor.php scripts
+            iframe.css('opacity', 0);
+            // open modal with contact form add new url
+            modal.show();
+            // modal close button click event
+            close.on('click', function(){
+                addButtonFunction();
+            });
+        });
+
+        //this ensures the data remains the same even after switching back from advanced tab to content tab
+        panel.$el.find( '.elementor-tab-control-content a' ).on( 'click',function(event){
+            pass_around_model( panel,model,view );
+        });
+
+        function editButtonFunction(){
             // loader add on modal
             modalLoading.addClass('loading');
             // insert src in iframe with edit link of selected form
@@ -43,66 +80,51 @@
                 // modal modal after completed all the tasks
                 modal.fadeOut(500);
             });
-        });
+        }
 
-        // form add new button element selector
-        let $elementAdd = panel.$el.find( '.void-cf7-add-form-btn' ).find( '#void-cf7-add-form-btn' );
-        // form add new button click event function
-        $elementAdd.on('click', function(e){
-            e.preventDefault();
-            // loader add on modal
-            modalLoading.addClass('loading');
-            // insert src in iframe with edit link of selected form
-            iframe.attr('src', voidCf7Admin.url+'admin.php?page=wpcf7-new');
-            // set opacity 0 to hide iframe untill it's load contents, opacity will be 1 after it's load content from modal-editor.php scripts
-            iframe.css('opacity', 0);
-            // open modal with contact form add new url
-            modal.show();
-            // modal close button click event
-            close.on('click', function(){
-                // element selector of form selector select2 from panel to avoid multiple search on DOM
-                var elCf7 = panel.$el.find('[data-setting="cf7"]');
-                // ajax request to admin-ajax for getting all contact form 7 from database
-                $.ajax({
-                    // url of ajax request, value of voidCf7Admin.ajaxUrl is localized during enqueue script
-                    url: voidCf7Admin.ajaxUrl,
-                    type: 'POST',
-                    data: {
-                        action: 'void_cf7_data'
-                    },
-                    // wp verify nonce automatically after sending nonce like this
-                    headers: {
-                        'X-WP-Nonce': voidCf7Admin.wpRestNonce
-                    },
-                    dataType: 'json',
-                    // success function of ajax request
-                    success: function (output) {
-                        // clear corrent option from form select select2
-                        elCf7.empty();
-                        // running loop by all from data after gettings it from ajax request
-                        $.each(output, function(index, value){
-                            // add option to form select select2 with new created from
-                            elCf7.append('<option value="'+index+'">'+value+'</option>');
-                        });
-                        //set already selected value
-                        elCf7.val( model.attributes.settings.attributes.cf7);
+        function addButtonFunction() {
+            // element selector of form selector select2 from panel to avoid multiple search on DOM
+            var elCf7 = panel.$el.find('[data-setting="cf7"]');
+            // ajax request to admin-ajax for getting all contact form 7 from database
+            $.ajax({
+                // url of ajax request, value of voidCf7Admin.ajaxUrl is localized during enqueue script
+                url: voidCf7Admin.ajaxUrl,
+                type: 'POST',
+                data: {
+                    action: 'void_cf7_data'
+                },
+                // wp verify nonce automatically after sending nonce like this
+                headers: {
+                    'X-WP-Nonce': voidCf7Admin.wpRestNonce
+                },
+                dataType: 'json',
+                // success function of ajax request
+                success: function (output) {
+                    // clear corrent option from form select select2
+                    elCf7.empty();
+                    // running loop by all from data after gettings it from ajax request
+                    $.each(output, function(index, value){
+                        // add option to form select select2 with new created from
+                        elCf7.append('<option value="'+index+'">'+value+'</option>');
+                    });
+                    //set already selected value
+                    elCf7.val( model.attributes.settings.attributes.cf7);
 
-                        // disable select2 of form select if there was no data
-                        if( elCf7.has('option').length == 0 ) {
-                            elCf7.attr('disabled', 'disabled');
-                        }else{
-                            elCf7.removeAttr('disabled');
-                        }
-                    },
-                    // error function of ajax request
-                    error: function (e) {
-                        console.log(e);
-                    },
-                });
-                // hide after completed all the actions        
-                modal.fadeOut(500);
+                    // disable select2 of form select if there was no data
+                    if( elCf7.has('option').length == 0 ) {
+                        elCf7.attr('disabled', 'disabled');
+                    }else{
+                        elCf7.removeAttr('disabled');
+                    }
+                },
+                // error function of ajax request
+                error: function (e) {
+                    console.log(e);
+                },
             });
-        });
+            // hide after completed all the actions        
+            modal.fadeOut(500);
+        }
     };
 
     // inilialization of js hook on elementor frondend and editor panel
